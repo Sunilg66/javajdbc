8237019bc77129dee7b0e87e5766564ccf93daea
Sunilg66 <sunilkg251@gmail.com>
2021-08-07 21:48:44 +0530

    adding files

---
+++ jdbc/src/com/sunil/jdbc/constants/JdbcConstants.java
@@ -0,0 +1,9 @@
+package com.sunil.jdbc.constants;
+
+public class JdbcConstants {
+	
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "Sunilgksp@42";
+	public static final String URL = "jdbc:mysql://localhost:3306/april_transformers";
+
+}
